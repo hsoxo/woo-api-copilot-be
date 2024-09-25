2fa9d0681524ccd3d71711410e6705d806fa9e0a
Bolong He <ihebolong@gmail.com>
2024-09-24 23:29:21 -0700

    update

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
+
 from pydantic import BaseModel
 
 from get_response import inference
@@ -7,11 +10,25 @@ from get_response import inference
 # Create an instance of the FastAPI class
 app = FastAPI()
 
+# Set up CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # List of allowed origins
+    allow_credentials=True,  # Allows cookies and other credentials
+    allow_methods=["*"],  # Allows all HTTP methods (GET, POST, etc.)
+    allow_headers=["*"],  # Allows all headers
+)
+
+spa_path = "/web"
+
+app.mount("/", StaticFiles(directory=spa_path, html=True), name="spa")
+
+
 class Q(BaseModel):
     query: str
 
 
 # Define a route
-@app.post("/")
+@app.post("/api/ask")
 def read_root(q: Q):
     return inference(q.query)
